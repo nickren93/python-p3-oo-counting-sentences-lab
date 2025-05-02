f1f8b4ebf66e281525ac80bded6fc9495bef0412
Sitong <nickren93@gmail.com>
2025-05-01 23:41:02 -0400

    complete lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ""):
+    self.value = value
+
+  @property
+  def value(self):
+    """The value property"""
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    '''prints "The value must be a string." if not string.'''
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    if self.value[-1] == ".":
+      return True
+    else:
+      return False
+
+  def is_question(self):
+    if self.value[-1] == "?":
+      return True
+    else:
+      return False
+
+  def is_exclamation(self):
+    if self.value[-1] == "!":
+      return True
+    else:
+      return False
+    
+  def count_sentences(self):
+    updated_value = self.value.replace("?", ".").replace("!", ".")
+    value_list = updated_value.split(".")
+    updated_value_list = [ sentence for sentence in value_list if sentence != ""]
+    return len(updated_value_list)
